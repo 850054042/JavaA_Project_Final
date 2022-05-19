2cb709957679ec71fdbcd3c56440d9a2b2a18d34
LouDevic <12111801@mail.sustech.edu.cn>
2022-05-20 00:08:14 +0800

    把音乐加上了

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             menu.setVisible(true);
             //我先把加载菜单放在这里，之后是进入新游戏或存档的时候
             //直接从那里调用ChessGameFrame，这里就不用写了
-            MusicPlayer musicPlayer = new MusicPlayer("D://文件//伴奏//Bohemian Rhapsody.mp3");
+            MusicPlayer musicPlayer = new MusicPlayer("D://文件//伴奏//Soledad.mp3");
             musicPlayer.start();
         });
     }
