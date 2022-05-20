52ede95c33cab078ad8032186fbebd2a49255cf2
LouDevic <12111801@mail.sustech.edu.cn>
2022-05-20 11:27:36 +0800

    把音乐加上了

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             Menu menu = new Menu(360,760);
             menu.setVisible(true);
-            //我先把加载菜单放在这里，之后是进入新游戏或存档的时候
-            //直接从那里调用ChessGameFrame，这里就不用写了
             MusicPlayer musicPlayer = new MusicPlayer("D://文件//伴奏//Soledad.mp3");
             musicPlayer.start();
         });
